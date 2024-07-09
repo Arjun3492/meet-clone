9bb82bb54cb0ef15b5d097716d22e60319ef0672
Arjun3492 <arjun.kohli.330@gmail.com>
2024-07-10 02:44:21 +0530

    using external socket

--- context/socket.js
+++ context/socket.js
@@ -4,24 +4,22 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-  const socket = useContext(SocketContext)
-  return socket
-}
+  const socket = useContext(SocketContext);
+  return socket;
+};
 
-export const SocketProvider = (props) => {
-  const { children } = props;
+export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
     const connection = io();
-    console.log("socket connection", connection)
+    console.log("socket connection", connection);
     setSocket(connection);
-  }, []);
 
-  socket?.on('connect_error', async (err) => {
-    console.log("Error establishing socket", err)
-    await fetch('/meet-clone/api/socket')
-  })
+    return () => {
+      connection.disconnect();
+    };
+  }, []);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
